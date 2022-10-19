f29bb6c3d8baeb1671d964f5e1daecf7a8bc0b8a
AKASH-PRASAD7 <akash.csel016@bppimt.ac.in>
2022-10-19 13:01:15 +0530

    Added create and get methods in Users route

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const { type } = require("express/lib/response");
+const { users } = require("./Data/Users.json"); //data import
 const app = express();
 const port = 8082;
 app.use(express.json());
@@ -10,6 +12,75 @@ app.listen(port, () => {
 app.get("/", (req, res) => {
   res.status(200).json({
     msg: "Server Is Running !", //json()sends only json data
+  });
+});
+
+/**
+ * Route: Users
+ * Method: GET
+ * Description: Get all Users
+ * Acess: Public
+ * Paramenters: none
+ */
+app.get("/users", (req, res) => {
+  res.status(200).json({
+    sucess: true,
+    data: users,
+  });
+});
+
+/**
+ * Route: Users/:id
+ * Method: GET
+ * Description: Get Users by
+ * Acess: Public
+ * Paramenters: id
+ */
+
+app.get("/user/:id", (req, res) => {
+  const { id } = req.params;
+  const user = users.find((each) => each.id === id);
+  if (!user) {
+    return res.status(404).json({
+      sucess: false,
+      message: "User does not exist",
+    });
+  }
+  return res.status(200).json({
+    sucess: true,
+    data: user,
+  });
+});
+
+/**
+ * Route: Users
+ * Method: POST
+ * Description: Create new user
+ * Acess: Public
+ * Paramenters: none
+ */
+app.post("/users", (req, res) => {
+  const { id, name, surname, email, sub_type, sub_date } = req.body;
+  const user = users.find((each) => each.id === id);
+  if (user) {
+    return res.status(400).json({
+      sucess: false,
+      message: "User already exist with given ID",
+    });
+  }
+
+  users.push({
+    id,
+    name,
+    surname,
+    email,
+    sub_type,
+    sub_date,
+  });
+
+  return res.status(200).json({
+    success: true,
+    data: users,
   });
 });
 
